81b1b867fce6df74c397976ed3a639ecf129b9e3
Arjun Sharma <sharmaarjun9646@gmail.com>
2025-01-21 15:39:32 +0530

    Add files via upload

---
+++ preprocessor.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def preprocess(df,region_df):#receiving
+
+    #filtering of summer olympics
+    df=df[df['Season']=='Summer']
+    df=df.merge(region_df,on='NOC',how='left')
+    df.drop_duplicates(inplace=True)
+    df=pd.concat([df,pd.get_dummies(df['Medal'])],axis=1)
+    return df
